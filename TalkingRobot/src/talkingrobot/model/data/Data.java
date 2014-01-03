e8d015629ec882ee456639e71a8e159ba2500d7b
Germandrummer92 <Germandrummer92@gmail.com>
2014-01-03 17:21:46 +0100

    Interface Data initial commit

---
+++ TalkingRobot/src/talkingrobot/model/data/Data.java
@@ -0,0 +1,10 @@
+package talkingrobot.model.data;
+
+/**
+ * @author Daniel Draper
+ * @version 1.0
+ * Interface regarding the Data saving/loading methods. Implemented by every Class in the Data package that deals with files on the harddrive.
+ */
+public interface Data {
+
+}
