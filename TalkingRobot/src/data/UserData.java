4d1131b776289ad893ee06d991d58d5b3b3a9997
Germandrummer92 <Germandrummer92@gmail.com>
2014-01-04 14:19:49 +0100

    UserData implemented

--- TalkingRobot/src/data/UserData.java
+++ TalkingRobot/src/data/UserData.java
@@ -1,48 +1,186 @@
 package data;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Vector;
 
+import org.json.JSONException;
 import org.json.JSONObject;
+import com.google.gson.*;
 
+/**
+ * 
+ * @author Daniel Draper
+ * @version 1.0
+ * This Class represents the data for each user of the system. 
+ * @see Data
+ */
 public class UserData implements Data {
 
-  private Integer userID;
-
+  private int userID;
   private String userName;
-
   private boolean isStudent;
-
   private ArrayList <RecipeDatePair>requestedRecipes;
-
   private ArrayList <MealDatePair> acceptedSuggestions;
-
   private Date lastAccess;
-
   private JSONObject userJSON;
 
-    public Vector  myRecipeDatePair;
-    public Vector  myMealDatePair;
+  /**
+   * Creates a new UserData object for a new User.
+   * @param userID ID of the new User
+   * @param userName Name of the new User
+   * @param isStudent if the new User is a Student.
+   */
+  public UserData(int userID, String userName, boolean isStudent) {
+	  this.userID = userID;
+	  this.userName = userName;
+	  this.isStudent = isStudent;
+	  lastAccess = new Date();
+	  requestedRecipes = new ArrayList<RecipeDatePair>();
+	  acceptedSuggestions = new ArrayList<MealDatePair>();
+  }
+  
 	@Override
+    /**
+     * @see Data#generateJSON()
+     */
 	public String generateJSON() {
-		// TODO Auto-generated method stub
-		return null;
+		Gson creator; 
+		creator = new Gson();
+		return creator.toJson(this);
 	}
+	
 	@Override
+	/**
+	 * @see Data#createFromJSONText(String jsonString)
+	 */
 	public void createFromJSONText(String jsonString) {
-		// TODO Auto-generated method stub
-		
+		Gson creator; 
+		creator = new Gson();
+		UserData newData = creator.fromJson(jsonString, this.getClass());
+		userID = newData.getUserID();
+		userName = newData.getUserName();
+		isStudent = newData.isStudent();
+		lastAccess =  newData.getLastAccess();
+		requestedRecipes = newData.getRequestedRecipes();
+		acceptedSuggestions = newData.getAcceptedSuggestions();
 	}
+		
 	@Override
+	/**
+	 * @see Data#writeFile()
+	 */
 	public void writeFile() {
-		// TODO Auto-generated method stub
-		
+		String pathname = "JsonResources/UserData/" + userID + ".json";
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter(pathname, "UTF-8");
+			writer.println(this.generateJSON());
+			writer.close();
+		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
 	}
 	@Override
+	/**
+	 * @see Data#getJson()
+	 */
 	public JSONObject getJson() {
-		// TODO Auto-generated method stub
-		return null;
+		JSONObject object = null;
+		try {
+			object = new JSONObject(this.generateJSON());
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		return object;
+	}
+	
+	/**
+	 * @return the userID
+	 */
+	public Integer getUserID() {
+		return userID;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @return the isStudent
+	 */
+	public boolean isStudent() {
+		return isStudent;
+	}
+	/**
+	 * @return the requestedRecipes
+	 */
+	public ArrayList<RecipeDatePair> getRequestedRecipes() {
+		return requestedRecipes;
+	}
+	/**
+	 * @return the acceptedSuggestions
+	 */
+	public ArrayList<MealDatePair> getAcceptedSuggestions() {
+		return acceptedSuggestions;
+	}
+	/**
+	 * @return the lastAccess
+	 */
+	public Date getLastAccess() {
+		return lastAccess;
+	}
+	/**
+	 * @return the userJSON
+	 */
+	public JSONObject getUserJSON() {
+		return userJSON;
+	}
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(Integer userID) {
+		this.userID = userID;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @param isStudent the isStudent to set
+	 */
+	public void setStudent(boolean isStudent) {
+		this.isStudent = isStudent;
+	}
+	/**
+	 * @param requestedRecipes the requestedRecipes to set
+	 */
+	public void setRequestedRecipes(ArrayList<RecipeDatePair> requestedRecipes) {
+		this.requestedRecipes = requestedRecipes;
+	}
+	/**
+	 * @param acceptedSuggestions the acceptedSuggestions to set
+	 */
+	public void setAcceptedSuggestions(ArrayList<MealDatePair> acceptedSuggestions) {
+		this.acceptedSuggestions = acceptedSuggestions;
+	}
+	/**
+	 * @param lastAccess the lastAccess to set
+	 */
+	public void setLastAccess(Date lastAccess) {
+		this.lastAccess = lastAccess;
+	}
+	/**
+	 * @param userJSON the userJSON to set
+	 */
+	public void setUserJSON(JSONObject userJSON) {
+		this.userJSON = userJSON;
 	}
 
 }
