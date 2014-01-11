6cfbfa8c0e7f43c3c8ac4d028923d529abd4d800
luizhsilva <luigi.hss@gmail.com>
2014-01-11 20:50:20 +0100

    implement RecipeData and KeywordData

--- TalkingRobot/src/data/KeywordData.java
+++ TalkingRobot/src/data/KeywordData.java
@@ -1,44 +1,171 @@
 package data;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import com.google.gson.Gson;
 
 import dm.DialogState;
 
+/**
+ * 
+ * @author Luiz Henrique Soares Silva
+ * @version 1.0
+ * 
+ * This class describes and implements a Keyword Data.
+ *
+ */
 public class KeywordData implements Data {
 
-  private Integer wordID;
+	private Integer wordID;
 
-  private String word;
+  	private String word;
 
-  private DialogState dialogState;
+  	private DialogState dialogState;
 
-  private JSONObject keywordJSON;
+  	private JSONObject keywordJSON;
 
-  public Integer priority;
+  	private Integer priority;
 
-  public Data dataReference;
+  	private Data dataReference;
 
-@Override
-public String generateJSON() {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public void createFromJSONText(String jsonString) {
-	// TODO Auto-generated method stub
+	@Override
+	public String generateJSON() {
+		Gson creator = new Gson();
+		return creator.toJson(this);
+	}
 	
-}
-
-@Override
-public void writeFile() {
-	// TODO Auto-generated method stub
+	@Override
+	public void createFromJSONText(String jsonString) {
+		Gson creator;
+		creator =  new Gson();
+		KeywordData newData = creator.fromJson(jsonString, this.getClass());
+		this.wordID = newData.getWordID();
+		this.word = newData.getWord();
+		this.dialogState = newData.getDialogState();
+		this.priority = newData.getPriority();
+		this.dataReference = newData.getDataReference();
+		
+	}
 	
-}
+	@Override
+	public void writeFile() {
+		String pathname = "resources/files/KeywordData/" + wordID + ".json";
+	    PrintWriter writer;
+	    try {
+	            writer = new PrintWriter(pathname, "UTF-8");
+	            writer.println(this.generateJSON());
+	            writer.close();
+	    } catch (FileNotFoundException | UnsupportedEncodingException e) {
+	            e.printStackTrace();
+	    }
+	}
+	
+	@Override
+	public JSONObject getJson() {
+		JSONObject object = null;
+	    try {
+	            object = new JSONObject(this.generateJSON());
+	    } catch (JSONException e) {
+	            e.printStackTrace();
+	    }
+	    return object;
+	}
 
-@Override
-public JSONObject getJson() {
-	// TODO Auto-generated method stub
-	return null;
-}
+	/**
+	 * 
+	 * @return the keyword's ID.
+	 */
+	public Integer getWordID() {
+		return wordID;
+	}
+
+	/**
+	 * 
+	 * @param wordID the keyword's ID to be set.
+	 */
+	public void setWordID(Integer wordID) {
+		this.wordID = wordID;
+	}
+	
+	/**
+	 * 
+	 * @return the keyword (as a String).
+	 */
+	public String getWord() {
+		return word;
+	}
+	
+	/**
+	 * 
+	 * @param word the keyword (as a String) to be set.
+	 */
+	public void setWord(String word) {
+		this.word = word;
+	}
+	
+	/**
+	 * 
+	 * @return the keyword's dialog state.
+	 */
+	public DialogState getDialogState() {
+		return dialogState;
+	}
+	
+	/**
+	 * 
+	 * @param dialogState the dialog state reference to be set.
+	 */
+	public void setDialogState(DialogState dialogState) {
+		this.dialogState = dialogState;
+	}
+	
+	/**
+	 * 
+	 * @return the Keyword's JSONObject.
+	 */
+	public JSONObject getKeywordJSON() {
+		return keywordJSON;
+	}
+	
+	/**
+	 * 
+	 * @param keywordJSON the Keyword's JSONObject to be set.
+	 */
+	public void setKeywordJSON(JSONObject keywordJSON) {
+		this.keywordJSON = keywordJSON;
+	}
+	
+	/**
+	 * 
+	 * @return the keyword's priority.
+	 */
+	public Integer getPriority() {
+		return priority;
+	}
+
+	/**
+	 * @param priority the priority of this keyword to be set.
+	 */
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+	
+	/**
+	 * @return the current Data Reference.
+	 */
+	public Data getDataReference() {
+		return dataReference;
+	}
+
+	/**
+	 * @param dataReference the Data Reference to be set.
+	 */
+	public void setDataReference(Data dataReference) {
+		this.dataReference = dataReference;
+	}
 
 }
--- TalkingRobot/src/data/RecipeData.java
+++ TalkingRobot/src/data/RecipeData.java
@@ -1,53 +1,168 @@
 package data;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.gson.Gson;
+
+/**
+ * 
+ * @author Luiz Henrique. Soares Silva
+ * @version 1.0
+ * This class describes and implements a Recipe Data.
+ *
+ */
 public class RecipeData implements Data {
 
-  private Integer recipeID;
+	private Integer recipeID;
 
-  private String recipeName;
+	private String recipeName;
 
-  private Integer numOfSteps;
+	private Integer numOfSteps;
 
-  private ArrayList<IngredientData> ingredients;
+	private ArrayList<IngredientData> ingredients;
 
-  private ArrayList<RecipeStepData> steps;
+	private ArrayList<RecipeStepData> steps;
 
-  private ArrayList<ToolData> tools;
-
-  private UserData creator;
-
-  private String originalCountry;
-
-  private JSONObject recipeJSON;
-
-  private MealCategoryData mealCategory;
-
-@Override
-public String generateJSON() {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public void createFromJSONText(String jsonString) {
-	// TODO Auto-generated method stub
+	private ArrayList<ToolData> tools;
 	
-}
-
-@Override
-public void writeFile() {
-	// TODO Auto-generated method stub
+	private UserData creator;
 	
-}
+	private String originalCountry;
+	
+	private JSONObject recipeJSON;
+	
+	private MealCategoryData mealCategory;
 
 @Override
-public JSONObject getJson() {
-	// TODO Auto-generated method stub
-	return null;
-}
+	public String generateJSON() {
+		Gson creator = new Gson();
+		return creator.toJson(this);
+	}
+
+	@Override
+	public void createFromJSONText(String jsonString) {
+		Gson creator;
+		creator =  new Gson();
+		RecipeData newData = creator.fromJson(jsonString, this.getClass());
+		this.recipeID = newData.getRecipeID();
+		this.recipeName = newData.getRecipeName();
+		this.numOfSteps = newData.getNumOfSteps();
+		this.ingredients = newData.getIngredients();
+		this.steps = newData.getSteps();
+		this.tools = newData.getTools();
+		this.creator = newData.getCreator();
+		this.originalCountry = newData.getOriginalCountry();
+		this.mealCategory = newData.getMealCategory();
+	}
+	
+	@Override
+	public void writeFile() {
+		String pathname = "resources/files/KeywordData/" + recipeID + ".json";
+	    PrintWriter writer;
+	    try {
+	            writer = new PrintWriter(pathname, "UTF-8");
+	            writer.println(this.generateJSON());
+	            writer.close();
+	    } catch (FileNotFoundException | UnsupportedEncodingException e) {
+	            e.printStackTrace();
+	    }
+	}
+	
+	@Override
+	public JSONObject getJson() {
+		JSONObject object = null;
+	    try {
+	            object = new JSONObject(this.generateJSON());
+	    } catch (JSONException e) {
+	            e.printStackTrace();
+	    }
+	    return object;
+	}
+	
+	public Integer getRecipeID() {
+		return recipeID;
+	}
+	
+	public void setRecipeID(Integer recipeID) {
+		this.recipeID = recipeID;
+	}
+	
+	public String getRecipeName() {
+		return recipeName;
+	}
+	
+	public void setRecipeName(String recipeName) {
+		this.recipeName = recipeName;
+	}
+	
+	public Integer getNumOfSteps() {
+		return numOfSteps;
+	}
+	
+	public void setNumOfSteps(Integer numOfSteps) {
+		this.numOfSteps = numOfSteps;
+	}
+	
+	public ArrayList<IngredientData> getIngredients() {
+		return ingredients;
+	}
+	
+	public void setIngredients(ArrayList<IngredientData> ingredients) {
+		this.ingredients = ingredients;
+	}
+	
+	public ArrayList<RecipeStepData> getSteps() {
+		return steps;
+	}
+	
+	public void setSteps(ArrayList<RecipeStepData> steps) {
+		this.steps = steps;
+	}
+	
+	public ArrayList<ToolData> getTools() {
+		return tools;
+	}
+	
+	public void setTools(ArrayList<ToolData> tools) {
+		this.tools = tools;
+	}
+	
+	public UserData getCreator() {
+		return creator;
+	}
+	
+	public void setCreator(UserData creator) {
+		this.creator = creator;
+	}
+	
+	public String getOriginalCountry() {
+		return originalCountry;
+	}
+	
+	public void setOriginalCountry(String originalCountry) {
+		this.originalCountry = originalCountry;
+	}
+	
+	public JSONObject getRecipeJSON() {
+		return recipeJSON;
+	}
+	
+	public void setRecipeJSON(JSONObject recipeJSON) {
+		this.recipeJSON = recipeJSON;
+	}
+	
+	public MealCategoryData getMealCategory() {
+		return mealCategory;
+	}
+	
+	public void setMealCategory(MealCategoryData mealCategory) {
+		this.mealCategory = mealCategory;
+	}
 
 }
