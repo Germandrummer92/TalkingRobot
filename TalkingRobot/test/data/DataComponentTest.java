1e9b6c1e4f6ef8228839bf9eaa1d939377111b0b
Aleksandar Andonov <alexaand@gmail.com>
2014-01-12 13:11:21 +0100

    javadoc changes - read commit before! :D

--- TalkingRobot/test/data/DataComponentTest.java
+++ TalkingRobot/test/data/DataComponentTest.java
@@ -10,7 +10,7 @@ public abstract class DataComponentTest {
 	static String testDataPath;
 
 	/**
-	 * Write the robot data in a file, create a new RobotData object from this file and
+	 * Write the data in a file, create a new data object from this file and
 	 * test if the two objects are identical.
 	 */
 	@Test
