a8532d32e4e5b8a68e254ac9de31dcf019400c15
Aleksandar Andonov <alexaand@gmail.com>
2014-01-13 23:49:57 +0100

    resources\files directory structure
    With dummy files which are to be deleted when the directories are not
    empty anymore.

---
+++ TalkingRobot/resources/files/MealCategoryData/README.txt
@@ -0,0 +1,3 @@
+This is a dummy file in order for git to sync the directory structure!
+
+This file is to be deleted as the directory is not empty anymore!
--- TalkingRobot/src/data/RecipeData.java
+++ TalkingRobot/src/data/RecipeData.java
@@ -7,7 +7,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.nio.file.Files;
 import java.util.ArrayList;
 
 import org.json.JSONException;
