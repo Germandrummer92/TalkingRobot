0ed74b0fc9177df826ef1a56125e1fc3999ef08b
rosine <eragonfreak@web.de>
2014-01-19 00:06:29 +0100

    jdoc,defaultConstructor,analyze(),set grammarFile

--- TalkingRobot/src/nlu/KeywordAnalyzer.java
+++ TalkingRobot/src/nlu/KeywordAnalyzer.java
@@ -5,14 +5,32 @@ import java.util.List;
 
 import dm.Dictionary;
 
+/**
+ *
+ * @author Bettina Weller
+ * @version 1.0
+ * The class KeywordAnalyzer analyzes the input String regarding keywords which can indicate the
+ * next possible dialogstate.
+ */
 public class KeywordAnalyzer extends InputAnalyzer {
 
   private Dictionary currentDictionary;
 
-  protected File grammarFile; //keywordGrammar.gra
+  grammarFile = new File("resources/nlu/Phoenix/TalkingRobot/Grammar/keyword.gra/"); //keywordGrammar.gra
 
+  /**
+	 * Creates a new KeywordAnalyzer object.
+	 */
+	public KeywordAnalyzer() {
+	
+	}
+
+  /**
+   * @see InputAdapter#analyze()
+   */
   public List<String> analyze(String input) {
-  return null;
+    List<String> result = phoenix.operatePhoenix(grammarFile);
+		return result;
   }
 
 }
