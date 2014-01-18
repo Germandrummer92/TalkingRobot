a2ee5bf02211911c6f2b8a23a3e0d5af975dd26a
rosine <eragonfreak@web.de>
2014-01-18 23:57:34 +0100

    jdoc and default constructor added

--- TalkingRobot/src/nlu/ApprovalAnalyzer.java
+++ TalkingRobot/src/nlu/ApprovalAnalyzer.java
@@ -2,14 +2,30 @@ package nlu;
 import java.io.File;
 import java.util.List;
 
-
+/**
+ *
+ * @author Bettina Weller
+ * @version 1.0
+ * The class ApprovalAnalyzer analyzes if the input String is a statement of approval, disapproval or a
+ * decision. The latest one is only needes for errorhandling.
+ */
 public class ApprovalAnalyzer extends InputAnalyzer {
 
-  protected File grammarFile; //approvalGrammar.gra
+  grammarFile  = new File("resources/nlu/Phoenix/TalkingRobot/Grammar/approval.gra/"); //approvalGrammar.gra
+  
+  /**
+	 * Creates a new ApprovalAnalyzer object.
+	 */
+	public ApprovalAnalyzer() {
+	
+	}
 
-
-  public List<String> analyze(String input) {
-  return null;
-  }
+  /**
+     * @see InputAdapter#analyze()
+     */
+	public List<String> analyze(String input) {
+		List<String> result = phoenix.operatePhoenix(grammarFile);
+		return result;
+	}
 
 }
