59703d8e198bb23ebceae900f312373afbacc899
Aleksandar Andonov <alexaand@gmail.com>
2014-01-19 20:22:21 +0100

    Canteen Dialog implemented
    Issue: We need the doc from the mensa to parse the information from the
    canteen in a Canteen object and then use it at the dialogs.

--- TalkingRobot/src/dm/CanteenDialog.java
+++ TalkingRobot/src/dm/CanteenDialog.java
@@ -1,14 +1,35 @@
 package dm;
 
-import java.util.List;
-
+/**
+ * This class represents a canteenDialog and as such has a certain Canteen object
+ * associated with it.
+ * @author Aleksandar Andonov
+ * @version 1.0
+ */
 public abstract class CanteenDialog extends Dialog {
 
   protected Canteen currentCanteen;
 
-  public abstract void updateState(List<String> keywords, List<String> terms);
-
-  private void loadCanteeenData() {
+  /**
+   * Creates a new CanteenDialog at the given canteen which is part of the session given
+   * @param session The session which this dialog is part of.
+   * @param currentCanteen The canteen where this dialog is taking place.
+   */
+  public CanteenDialog(Session session, Canteen currentCanteen) {
+	  //TODO The canteen specs document is needed in order to parse tha data to
+	  //canteen object and load it into the system
+	  super(session);
+	  this.currentCanteen = currentCanteen;
   }
+
+  public Canteen getCurrentCanteen() {
+	  return currentCanteen;
+  }
+
+  public void setCurrentCanteen(Canteen currentCanteen) {
+	  this.currentCanteen = currentCanteen;
+  }
+  
+  
 
 }
