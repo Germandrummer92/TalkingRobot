74dc67527ba641ad7ded7ad3ac4b58ce810a85cc
narigaoka <yaten.kou@hotmail.de>
2014-01-20 00:29:26 +0100

    NLUPhase implemented; type of nluResult changed to List<List<String>> (List<String>[] is not allowed); type of nluResult also changed in DMPhase and Main

--- TalkingRobot/src/nlu/NLUPhase.java
+++ TalkingRobot/src/nlu/NLUPhase.java
@@ -4,6 +4,12 @@ import generalControl.Phase;
 
 import java.util.List;
 
+/**
+ * 
+ * @author Meng Meng Yan
+ * @version 1.0
+ * The class coordinates the NLUPhase and decides which analyzers are to be used.
+ */
 public class NLUPhase extends Phase {
 
   private InputAnalyzer termAnalyzer;
@@ -14,17 +20,38 @@ public class NLUPhase extends Phase {
 
   private InputAnalyzer approvalAnalyzer;
   
-
+  /**
+   * sets the nluResult in the class main.
+   * @param main: the main class of this system
+   */
   public void setPhaseResult(Main main) {
+	  String userInput = main.getAsrResult();
+	  List<List<String>> nluResult = null;
+	  
+	  nluResult.set(0, kwAnalyzer.analyze(userInput));
+	  nluResult.set(1, termAnalyzer.analyze(userInput));
+	  
+	  if(nluResult.get(0).isEmpty()) {
+		  nluResult.set(2, possibleKwAnalyzer.analyze(userInput));
+	  }
+	  
+	  nluResult.set(3, approvalAnalyzer.analyze(userInput));
+
+      main.setNluResult(nluResult);
   }
 
-  //Das bitte nicht aendern, die DmPhase sollte immer geoffnet sein, sonst muessten wir die Daten immer wieder laden, das ist glaub ich nicht so gut.
+  /**
+   * returns the next phase the system must enter.
+   * The DM-phase always follows after the NLU-phase
+   */
   protected Phase nextPhase(Main main) {
   		return main.getDmPhase();
   }
 
+  /*Die Methode scheint unnoetig zu sein
   private List<String> operateAnalyzer(String input, InputAnalyzer analyzer) {
   return null;
   }
-
+  */
+}
 }
