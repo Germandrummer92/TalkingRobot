edb86e31396e2f689cda10d29c4bcd05cae8199e
Xizhe Lian <xizhelian@googlemail.com>
2014-01-20 09:14:16 +0100

    begins to implement OutputGenerator
    there are some problems with the string and phrase and phrase class

--- TalkingRobot/src/nlg/OutputCreator.java
+++ TalkingRobot/src/nlg/OutputCreator.java
@@ -1,5 +1,12 @@
 package nlg;
 
+
+/**
+ * 
+ * @author Xizhe Lian, Luiz Henrique Soares Silva
+ * @version 0.1
+ */
+import java.util.ArrayList;
 import java.util.List;
 
 import dm.DialogState;
@@ -14,28 +21,48 @@ public class OutputCreator {
   private SocialComponent socialComponent;
   
   public String createOutput(DialogState dialogState) {
-	  
-	  Phrase phrase = getOutputKeyword(dialogState);
-	  //TODO Check for SocialComponent (if yes, addSocialComponent())
+	  generators = new ArrayList<Generator>();
+	  outputPhrases = new ArrayList<Phrase>();
+	  //Phrase phrase = getOutputKeyword(dialogState);
+	  //TODO Check for SocialComponent (if yes, addSocialComponent()) (How can we check?)
 	  
 	  return null;
   }
 
-  private Phrase getOutputKeyword(DialogState dialogState) {
+  /**
+   * I dont know how to decide which type belongs a token to
+   * for example: 'give me spagetti', then the string[] tokens will be {give, me, spagetti}
+   * I think if we can set the phrase type in dialogstate.getOutputKeywords will be nice
+   * Xizhe
+   * @param dialogState
+   */
+  private void toPhrase(DialogState dialogState) {
 	  String keywords = dialogState.getOutputKeyword();
 	  
-	  //TODO String to Phrase
+	  String delims = " "; // assume that exactly one space between each word
+	  String[] tokens = keywords.split(delims);
 	  
-	  return null;
+	  for(int i = 0; i < tokens.length; i++) {
+		  Phrase phrase = new Phrase();
+		  phrase.setPhraseString(tokens[i]);
+		  outputPhrases.add(phrase);
+	  }	  
+	
   }
 
+  /**
+   * When should we add social component?
+   * we need so database for social components // Xizhe  
+   * @param dialogState
+   * @return
+   */
   private String addSocialComponent(DialogState dialogState) {
 	  String keyword = dialogState.getOutputKeyword();
 	  ///TODO Sth
-	  Phrase phrase = this.getOutputKeyword(dialogState);
-	  String socialRemark = socialComponent.createSocialRemark(phrase);
+	 // Phrase phrase = this.toPhrase(dialogState);
+	//  String socialRemark = socialComponent.createSocialRemark(phrase);
 	  
-  return socialRemark;
+  return null;
   }
 
 }
