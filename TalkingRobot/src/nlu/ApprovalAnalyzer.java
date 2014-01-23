b7b3417b78ec47bd6bca0147d8245d24be2ff5f7
Meng Meng Yan <ubdns@student.kit.edu>
2014-01-23 08:34:32 +0100

    nlu PhoenixAdapter edited and Analyzers adjusted (by Bettina)

--- TalkingRobot/src/nlu/ApprovalAnalyzer.java
+++ TalkingRobot/src/nlu/ApprovalAnalyzer.java
@@ -7,7 +7,7 @@ import java.util.List;
  * @author Bettina Weller
  * @version 1.0
  * The class ApprovalAnalyzer analyzes if the input String is a statement of approval, disapproval or a
- * decision. The latest one is only needes for errorhandling.
+ * decision. The latest one is only needed for errorhandling.
  */
 public class ApprovalAnalyzer extends InputAnalyzer {
   
@@ -17,13 +17,14 @@ public class ApprovalAnalyzer extends InputAnalyzer {
 	public ApprovalAnalyzer() {
 		this.runParse = "run_parse_approval";
 		this.extractFlag = 1;
+		this.compile = new File ("resources/nlu/Phoenix/TalkingRobot/Approval/");
 	}
 
   /**
      * @see InputAdapter#analyze()
      */
 	public List<String> analyze(String input) {
-		List<String> result = phoenix.operatePhoenix(this.runParse, this.extractFlag);
+		List<String> result = phoenix.operatePhoenix(input, this.runParse, this.extractFlag, this.compile);
 		return result;
 	}
 
--- TalkingRobot/src/nlu/PhoenixAdapter.java
+++ TalkingRobot/src/nlu/PhoenixAdapter.java
@@ -1,13 +1,35 @@
 package nlu;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 
+ * @author Bettina Weller
+ * @version 1.0
+ * The class PhoenixAdapter is an adapter to use phoenix in a Java program.
+ */
 public class PhoenixAdapter {
           
-  public LinkedList<String> operatePhoenix(String runParse, int extractFlag) {
-  return new LinkedList<String>();
+  public LinkedList<String> operatePhoenix(String input, String runParse, int extractFlag, File compile) {
+	  try {
+		  Runtime.getRuntime().exec("tcsh");
+		  Runtime.getRuntime().exec("tcsh cd " + compile.getAbsolutePath());
+		  Runtime.getRuntime().exec("tcsh compile");
+		  Runtime.getRuntime().exec("tcsh cd ..");
+		  File file = new File("input");
+		  BufferedWriter output = new BufferedWriter(new FileWriter(file));
+		  output.write(input);
+		  output.close();
+		  Runtime.getRuntime().exec("tcsh " + runParse + " < input");
+	  } catch (IOException e) {
+		  e.printStackTrace();
+	  }
+	  return new LinkedList<String>();
   }
 
 }
