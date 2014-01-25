e180cb0f801794e505e92c800a2c64cb6eb9772e
meng meng <mengmeng-yan@hotmail.com>
2014-01-25 15:10:39 +0100

    nlu edited; PhoenixAdapter updated, keyword.gra improved

--- TalkingRobot/src/nlu/PhoenixAdapter.java
+++ TalkingRobot/src/nlu/PhoenixAdapter.java
@@ -1,12 +1,16 @@
 package nlu;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * 
@@ -17,26 +21,77 @@ import java.util.List;
 public class PhoenixAdapter {
           
   public LinkedList<String> operatePhoenix(String input, String runParse, int extractFlag, File compile) {
+	  LinkedList<String> phoenixOutput = new LinkedList<String>();
+	  
 	  try {
 		  Runtime rt = Runtime.getRuntime();
-		  rt.exec("/bin/tcsh compile", null, compile.getAbsoluteFile());
+		  Process compileGra = rt.exec("tcsh compile", null, compile.getAbsoluteFile());
 		  
-		  System.out.println("exec done \n");
+		  BufferedReader stdin1 = new BufferedReader(new InputStreamReader(compileGra.getInputStream()));
+		  BufferedReader stderr1 = new BufferedReader(new InputStreamReader(compileGra.getErrorStream()));
 		  
-		  File file = new File("resources/nlu/Phoenix/TalkingRobot/input");
-		  PrintWriter writer = new PrintWriter(file, "UTF-8");
-		  writer.println(input);
-		  writer.close();
+		  String phoenixLine = null;
+		  
+		  System.out.println("compile regular");
+		  while ((phoenixLine = stdin1.readLine()) != null) {
+			  phoenixOutput.add(phoenixLine);
+			  System.out.println(phoenixLine);
+		  }
+		  System.out.println("compile regular end");
+//		  
+		  System.out.println("compile error");
+		  while ((phoenixLine = stderr1.readLine()) != null) {
+			  System.out.println(phoenixLine);
+		  }
+		  System.out.println("compile error end");
+		  
+//		  compileGra.destroy();
+//
+//		  File file = new File("resources/nlu/Phoenix/TalkingRobot/input");
+//		  PrintWriter writer = new PrintWriter(file, "UTF-8");
+//		  writer.println(input);
+//		  writer.close();
 		  
 //		  BufferedWriter output = new BufferedWriter(new FileWriter(file));
 //		  output.write(input);
 //		  output.close();
-		  rt.exec("/bin/tcsh " + runParse + " < input", null, new File("resources/nlu/Phoenix"));
 		  
+		  
+
+//		  @SuppressWarnings("resource")
+//		Scanner sc = new Scanner(System.in);
+		  
+//		  Runtime rt2 = Runtime.getRuntime();
+//		  Process phoenix = rt2.exec("tcsh " + runParse + " < input", null, new File("resources/nlu/Phoenix/TalkingRobot"));
+		  
+		  ProcessBuilder phoenixBuilder = new ProcessBuilder("/bin/tcsh", "run_parse_keyword < input");
+		  phoenixBuilder.directory(new File("resources/nlu/Phoenix/TalkingRobot"));
+		  Process phoenix = phoenixBuilder.start();
+		  
+		  BufferedReader stdin2 = new BufferedReader(new InputStreamReader(phoenix.getInputStream()));
+		  BufferedReader stderr2 = new BufferedReader(new InputStreamReader(phoenix.getErrorStream()));
+		  
+		  String phoenixLine2 = null;
+		  
+		  System.out.println("run error");
+		  while ((phoenixLine2 = stderr2.readLine()) != null) {
+			  System.out.println(phoenixLine2);
+		  }
+		  System.out.println("run error end");
+
+		  System.out.println("run regular");
+		  while ((phoenixLine2 = stdin2.readLine()) != null) {
+			  phoenixOutput.add(phoenixLine2);
+			  System.out.println(phoenixLine2);
+		  }
+		  System.out.println("run regular end");
+		  
+		  
+		  System.out.println("exec done \n");
 	  } catch (IOException e) {
 		  e.printStackTrace();
 	  }
-	  return new LinkedList<String>();
+	  return phoenixOutput;
   }
 
 }
