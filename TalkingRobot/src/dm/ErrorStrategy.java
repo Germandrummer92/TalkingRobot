a8db5a59c66123450424667ebe899822d85fe948
narigaoka <yaten.kou@hotmail.de>
2014-01-29 23:57:47 +0100

    jdoc added

--- TalkingRobot/src/dm/ErrorStrategy.java
+++ TalkingRobot/src/dm/ErrorStrategy.java
@@ -6,23 +6,43 @@ import java.util.List;
  * 
  * @author Meng Meng Yan
  * @version 1.0
+ * template for the error strategy classes.
  */
 public abstract class ErrorStrategy {
 
-  private Integer counter = 0;
-
-  public abstract ErrorHandlingState handleError(List<String> errorWords);
-
-  public void clearCounter() {
-	  counter = 0;
-  }
-
-  public Integer getCounter() {
-	  return counter;
-  }
-
-  protected void riseCounter() {
-	  counter++;
-  }
-
+	 /**
+	  * indicates the number of times the certain strategy has been used in one 
+	  * errorhandling round.
+	  */
+	  protected Integer counter = 0;
+	
+	  /**
+	   * method to handle an error with the given list of words which might be keywords.
+	   * @param errorWords words which might be keywords
+	   * @return a special DialogState which contains the information 
+	   * needed by the NLG to create the next output
+	   */
+	  public abstract ErrorHandlingState handleError(List<String> errorWords);
+	
+	  /**
+	   * resets the counter to zero.
+	   */
+	  public void clearCounter() {
+		  counter = 0;
+	  }
+	
+	  /**
+	   * getter-method of counter.
+	   * @return the value of counter
+	   */
+	  public Integer getCounter() {
+		  return counter;
+	  }
+	
+	  /**
+	   * rises value of counter by one.
+	   */
+	  protected void riseCounter() {
+		  counter++;
+	  }
 }
