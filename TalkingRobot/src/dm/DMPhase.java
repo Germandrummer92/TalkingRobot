534f3852e79f1dfa8115a46de36628dc30811190
Meng Meng Yan <ubdns@student.kit.edu>
2014-01-29 22:17:14 +0100

    ErrorHandling edited in DMPhase and DialogManager; ErrorHandlingState implemented; enum ErrorHandling created

--- TalkingRobot/src/dm/DMPhase.java
+++ TalkingRobot/src/dm/DMPhase.java
@@ -28,31 +28,22 @@ public class DMPhase extends Phase {
    */
   public void setPhaseResult(Main main) {
 	  	//If nothing has been parsed, handle Error
-	  	ErrorHandlingState errorHandlingState = null;
-	  	if (main.getNluResult().get(0).isEmpty() 
-	  			&& main.getNluResult().get(1).isEmpty() 
-	  			&& main.getNluResult().get(3).isEmpty()) {
-	  		errorHandlingState = dialogManager.handleError(main.getNluResult().get(2));
-	  	} else if (dialogManager.getErrorState() != ErrorState.ZERO 
+	  	
+	  	if (dialogManager.getErrorState() != ErrorState.ZERO 
 	  			&& main.getNluResult().get(0).isEmpty() 
-	  			&& main.getNluResult().get(1).isEmpty()
 	  			&& !main.getNluResult().get(3).isEmpty()) {
 	  		/* in this case the system gave the user a choice or used a method of verification
 	  		 * and expects an answer. Though, if the user answers with a sentence which contains
-	  		 * a keyword or a term, this case can be ignored and the system can continue with its
+	  		 * a keyword, this case can be ignored and the system can continue with its
 	  		 * previous task.
 	  		 */
-	  		errorHandlingState = dialogManager.handleError(main.getNluResult().get(3));
-	  	}
-
-
-	  	dialogManager.updateDialog(main.getNluResult().get(0), main.getNluResult().get(1),
-			main.getNluResult().get(3));
-	  	if(errorHandlingState == null) {
-		 	main.setDmResult(dialogManager.getCurrentDialog().getCurrentDialogState());
+	  		dialogManager.handleError(main.getNluResult().get(3));
 	  	} else {
-	  		main.setDmResult(errorHandlingState);
+		  	dialogManager.updateDialog(main.getNluResult().get(0), main.getNluResult().get(1),
+				main.getNluResult().get(3));
 	  	}
+		main.setDmResult(dialogManager.getCurrentDialog().getCurrentDialogState());
+
   }
 
   /**
---
+++ TalkingRobot/src/dm/ErrorHandling.java
@@ -0,0 +1,22 @@
+package dm;
+
+/**
+ * @author Meng Meng Yan
+ * @version 1.0
+ * the different possible error handling strategies.
+ */
+public enum ErrorHandling {
+	
+	REPEAT,
+	
+	REPHRASE,
+	
+	RESTART,
+	
+	CHOICE,
+	
+	Explicit_Verification,
+	
+	Indirect_Verification;
+
+}
