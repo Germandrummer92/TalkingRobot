0cb95ae5bd2c40591694320ac268957e5d23de8a
narigaoka <yaten.kou@hotmail.de>
2014-01-29 23:59:21 +0100

    RephraseStrategy implemented

--- TalkingRobot/src/dm/RephraseStrategy.java
+++ TalkingRobot/src/dm/RephraseStrategy.java
@@ -2,14 +2,30 @@ package dm;
 
 import java.util.List;
 
+/**
+ * 
+ * @author Meng Meng Yan
+ * @version 1.0
+ * The rephrase strategy: asks the user to rephrase his previous input
+ */
 public class RephraseStrategy extends ErrorStrategy {
 
-  private ErrorHandlingState rephraseState;
-
-@Override
-public ErrorHandlingState handleError(List<String> errorWords) {
-	// TODO Auto-generated method stub
-	return null;
-}
+	private ErrorHandlingState rephraseState;
+	
+	/**
+	 * Constructor of RephraseStrategy.
+	 */
+	public RephraseStrategy() {
+		this.rephraseState = new ErrorHandlingState(true, ErrorHandling.REPHRASE, "rephrase");
+	}
+	
+	/**
+	 * @see ErrorStrategy#handleError(List)
+	 */
+	@Override
+	public ErrorHandlingState handleError(List<String> errorWords) {
+		this.riseCounter();
+		return rephraseState;
+	}
 
 }
