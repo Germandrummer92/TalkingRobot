a764fba8ff7546415f43bdcf5f3eb509f9fb1c44
bettina <bettinaweller@web.de>
2014-01-30 19:58:33 +0100

    tests added

--- TalkingRobot/test/nlu/PhoenixAdapterTest.java
+++ TalkingRobot/test/nlu/PhoenixAdapterTest.java
@@ -1,5 +1,152 @@
 package nlu;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PhoenixAdapterTest {
+	private PhoenixAdapter phoenix;
+	
+	private void setInput(String input) {
+  		File file = new File("resources/nlu/Phoenix/TalkingRobot/input");
+  		PrintWriter writer;
+  		try {
+  			writer = new PrintWriter(file, "UTF-8");
+  			writer.println(input);
+  			writer.close();
+  		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+  			e.printStackTrace();
+  		}
+	}
+	
+	@Before
+	public void setUp() {
+		this.phoenix = new PhoenixAdapter();
+	}
+	
+	/**
+	 * checks the result of "For sandwiches you need toast and salad" for term.gra
+	 */
+	@Test
+	public void term1() { 
+		setInput("For sandwiches you need toast and salad");
+		LinkedList <String> result = phoenix.operatePhoenix("run_parse_term", 0, 
+				new File("resources/nlu/Phoenix/TalkingRobot/Term/"));
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("toast");
+		compare.add("salad");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result of "My very pretty name is Nicole" for term.gra
+	 */
+	@Test
+	public void term2() { 
+		setInput("My very pretty name is Nicole");
+		LinkedList <String> result = phoenix.operatePhoenix("run_parse_term", 0, 
+				new File("resources/nlu/Phoenix/TalkingRobot/Term/"));
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("nicole");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result of "I want to go to the cantean" for possibleKw.gra
+	 */
+	@Test
+	public void possibleKw1() { 
+		setInput("I want to go to the cantean");
+		LinkedList <String> result = phoenix.operatePhoenix("run_parse_possiblekw", 0,
+				new File("resources/nlu/Phoenix/TalkingRobot/PossibleKw/"));
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("cantean");
+		compare.add("canteen");
+		compare.add("1");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result of "that is al" for possibleKw.gra
+	 */
+	@Test
+	public void possibleKw2() { 
+		setInput("I want to go to the cantean");
+		LinkedList <String> result = phoenix.operatePhoenix("run_parse_possiblekw", 0,
+				new File("resources/nlu/Phoenix/TalkingRobot/PossibleKw/"));
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("that is al");
+		compare.add("that is all");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result of "Yes that is right" for approval.gra
+	 */
+	@Test
+	public void approval1() { 
+		setInput("Yes that is right");
+		LinkedList <String> result = phoenix.operatePhoenix("run_parse_approval", 1,
+				new File("resources/nlu/Phoenix/TalkingRobot/Approval/"));
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("yes");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result of "It was the first option" for approval.gra
+	 */
+	@Test
+	public void approval2() { 
+		setInput("It was the first option");
+		LinkedList <String> result = phoenix.operatePhoenix("run_parse_approval", 1,
+				new File("resources/nlu/Phoenix/TalkingRobot/Approval/"));
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("first");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result of "Could you recommend me something to eat at the canteen" for keyword.gra
+	 */
+	@Test
+	public void keyword1() { 
+		setInput("Could you recommend me something to eat at the canteen");
+		LinkedList <String> result = phoenix.operatePhoenix("run_parse_keyword", 1,
+				new File("resources/nlu/Phoenix/TalkingRobot/Keyword/"));
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("canteen");
+		compare.add("recommendation");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result of "Could you get me the knife from the cupboard" for keyword.gra
+	 */
+	@Test
+	public void keyword2() { 
+		setInput("Could you get me the knife from the cupboard");
+		LinkedList <String> result = phoenix.operatePhoenix("run_parse_keyword", 1,
+				new File("resources/nlu/Phoenix/TalkingRobot/Keyword/"));
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("bring");
+		compare.add("knife");
+		compare.add("cupboard");
+		assertEquals(compare, result);
+	}
+	
+	@After
+	public void tearDown() {
+		this.phoenix = null;
+	}
+	
 
 }
