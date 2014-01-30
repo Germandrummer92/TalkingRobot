2f60ae43acc48b2771f615d6126be4fc8a0d9e8b
bettina <bettinaweller@web.de>
2014-01-30 22:27:29 +0100

    some very simple tests

--- TalkingRobot/test/nlu/ApprovalAnalyzerTest.java
+++ TalkingRobot/test/nlu/ApprovalAnalyzerTest.java
@@ -1,12 +1,63 @@
 package nlu;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
+
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class ApprovalAnalyzerTest {
 	private ApprovalAnalyzer approval;
 
+	private void setInput(String input) {
+  		File file = new File("resources/nlu/Phoenix/TalkingRobot/input");
+  		PrintWriter writer;
+  		try {
+  			writer = new PrintWriter(file, "UTF-8");
+  			writer.println(input);
+  			writer.close();
+  		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+  			e.printStackTrace();
+  		}
+	}
+	
 	@Before
-	private void setUp() {
+	public void setUp() {
 		this.approval = new ApprovalAnalyzer();
+	}
+	
+	/**
+	 * checks the result for "Yes that is right"
+	 */
+	@Test
+	public void yes() {
+		setInput("Yes that is right");
+		LinkedList<String> result = (LinkedList<String>) approval.analyze("Yes that is right");
+		LinkedList<String> compare = new LinkedList<String>();
+		compare.add("yes");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result for "It was the first option"
+	 */
+	@Test
+	public void approval2() { 
+		setInput("It was the first option");
+		LinkedList <String> result = (LinkedList<String>) approval.analyze("Yes that is right");
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("first");
+		assertEquals(compare, result);
+	}
+	
+	@After
+	public void tearDown() {
+		this.approval = null;
 	}
 }
--- TalkingRobot/test/nlu/KeywordAnalyzerTest.java
+++ TalkingRobot/test/nlu/KeywordAnalyzerTest.java
@@ -1,14 +1,62 @@
 package nlu;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class KeywordAnalyzerTest {
 	private KeywordAnalyzer keyword;
 	
+	private void setInput(String input) {
+  		File file = new File("resources/nlu/Phoenix/TalkingRobot/input");
+  		PrintWriter writer;
+  		try {
+  			writer = new PrintWriter(file, "UTF-8");
+  			writer.println(input);
+  			writer.close();
+  		} catch (FileNotFoundException | UnsupportedEncodingException e) {
+  			e.printStackTrace();
+  		}
+	}
+	
 	@Before
-	private void setUp() {
+	public void setUp() {
 		this.keyword = new KeywordAnalyzer();
+	}
+	
+	/**
+	 * checks the result for "Could you recommend me something to eat at the canteen"
+	 */
+	@Test
+	public void canteenRecommendation() { 
+		setInput("Could you recommend me something to eat at the canteen");
+		LinkedList <String> result = (LinkedList<String>) keyword.analyze("Could you recommend me something to eat at the canteen");
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("recommendation");
+		compare.add("canteen");
+		assertEquals(compare, result);
+	}
+	
+	/**
+	 * checks the result for "Could you get me the knife from the cupboard"
+	 */
+	@Test
+	public void keyword2() { 
+		setInput("Could you get me the knife from the cupboard");
+		LinkedList <String> result = (LinkedList<String>) keyword.analyze("Could you get me the knife from the cupboard");
+		LinkedList <String> compare = new LinkedList<String>();
+		compare.add("bring");
+		compare.add("knife");
+		compare.add("cupboard");
+		assertEquals(compare, result);
 	}
 
 	@After
