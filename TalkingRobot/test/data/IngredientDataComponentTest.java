b7aff877e87a0d08776668e17c9f31717d08c0c2
Germandrummer92 <Germandrummer92@gmail.com>
2014-02-01 15:31:32 +0100

    fixed Path in test

--- TalkingRobot/test/data/IngredientDataComponentTest.java
+++ TalkingRobot/test/data/IngredientDataComponentTest.java
@@ -4,12 +4,17 @@ import java.io.File;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-
+/**
+ * Tests the IngredientDataClass.
+ * @author Daniel Draper
+ * @version 1.0
+ *
+ */
 public class IngredientDataComponentTest extends DataComponentTest{
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		testData = new IngredientData("salt", "Cabinet 1");
-		testDataPath = "resources/files/IngredinetData/";
+		testDataPath = "resources/files/IngredientData/";
 		testDataClone = new IngredientData(testData.generateJSON(), 2);
 	}
 	
