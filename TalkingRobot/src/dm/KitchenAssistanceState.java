f3629c99d9a8aff90790eb78cc07855550b8f543
Germandrummer92 <Germandrummer92@gmail.com>
2014-02-12 13:45:54 +0100

    fixed error

--- TalkingRobot/src/dm/KitchenAssistanceState.java
+++ TalkingRobot/src/dm/KitchenAssistanceState.java
@@ -39,16 +39,16 @@ public class KitchenAssistanceState extends DialogState {
 		return null;
 	case KA_TELL_INGREDIENT_FOUND:
 		setQuestion(false);
-		String output =  "<" + ((Ingredient)((KitchenAssistanceDialog)currentDialog).getRequestedObject()).getIngredientData().getIngredientName() + ">";
-		output = output + ",{" + ((KitchenAssistanceDialog)currentDialog).getRequestedObjectName() + "}";
+		String output =  "<" + ((KitchenAssistanceDialog)currentDialog).getRequestedObjectName() + ">";
+		output = output + ",{" + ((Ingredient)((KitchenAssistanceDialog)currentDialog).getRequestedObject()).getIngredientData().getIngredientLocation() + "}";
 		return output;
 	case KA_TELL_INGREDIENT_NOT_FOUND:
 		setQuestion(false);
 		return "<" + ((KitchenAssistanceDialog)currentDialog).getRequestedObjectName() + ">";
 	case KA_TELL_TOOL_FOUND:
 		setQuestion(false);
-		String output2 =  "<" + ((Tool)((KitchenAssistanceDialog)currentDialog).getRequestedObject()).getToolData().getToolName() + ">";
-		output = output2 + ",{" + ((KitchenAssistanceDialog)currentDialog).getRequestedObjectName() + "}";
+		String output2 =  "<" + ((KitchenAssistanceDialog)currentDialog).getRequestedObjectName() + ">";
+		output2 = output2 + ",{" + ((Tool)((KitchenAssistanceDialog)currentDialog).getRequestedObject()).getToolData().getLocation() + "}";
 		return output2;
 	case KA_TELL_TOOL_NOT_FOUND:
 		setQuestion(false);
