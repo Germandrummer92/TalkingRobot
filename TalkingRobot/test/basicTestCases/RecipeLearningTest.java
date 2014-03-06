65674a6c6f37e66d222beaf9c316a8b2829adeab
narigaoka <yue.hong@hotmail.de>
2014-03-06 18:08:46 +0100

    edited

--- TalkingRobot/test/basicTestCases/RecipeLearningTest.java
+++ TalkingRobot/test/basicTestCases/RecipeLearningTest.java
@@ -2,6 +2,9 @@ package basicTestCases;
 
 
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
 import generalControl.Main;
 
 import org.junit.Test;
@@ -28,10 +31,19 @@ public class RecipeLearningTest extends BasicTest {
 		userInput.add("that is all");
 		
 		userInput.add("we need a knife");
+		userInput.add("that was the last");
+		
+		userInput.add("you first need to laugh");
+		userInput.add("then you also need to smile");
+		userInput.add("the last step is to grin");
+		
+		userInput.add("tell me about lol");
+		
 		this.runMainActivityWithTestInput(userInput);
 		
-//		assertTrue(Main.giveMain().getNlgResult().contains("know you") 
-//				|| Main.giveMain().getNlgResult().contains("profile has been saved"));
+		File newRecipe = new File("resources/files/RecipeData/" + recipeDataCount + ".json");
+		assertTrue(newRecipe.exists() 
+				&& Main.giveMain().getNlgResult().contains("to prepare this recipe") );
 	}
 
 }
