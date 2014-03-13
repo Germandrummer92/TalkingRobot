f96c59735e1244b4613aebd440b4169fc2a2f4b7
narigaoka <yue.hong@hotmail.de>
2014-03-13 19:09:36 +0100

    modifications

--- TalkingRobot/test/errorHandlingTests/RestartCanteenTest.java
+++ TalkingRobot/test/errorHandlingTests/RestartCanteenTest.java
@@ -9,50 +9,50 @@ import basicTestCases.BasicTest;
 
 public class RestartCanteenTest extends BasicTest {
 
-	/**
-	 * Testing restart, rephrase and restart strategy in CI
-	 */
-	@Test
-	public void reTest1() {
-		userInput.add("hi");
-		userInput.add("my name is meng meng");
-		userInput.add("i want to go to the canteen");
-		userInput.add("errorhandling");
-		userInput.add("errorhandling");
-		userInput.add("errorhandling");
-		userInput.add("errorhandling");
-		userInput.add("handle the error");
-		userInput.add("handle the error somehow");
-		userInput.add("handle this error");
-		userInput.add("yes");
-		
-		this.runMainActivityWithTestInput(userInput);
-		
-		assertTrue(true);
-	}
-	
-	/**
-	 * Testing restart, rephrase and restart strategy in CI
-	 */
-	@Test
-	public void reTest2() {
-		userInput.add("hi");
-		userInput.add("my name is meng meng");
-		userInput.add("i want to go to the canteen");
-		userInput.add("can i eat at line one");
-		userInput.add("errorhandling");
-		userInput.add("errorhandling");
-		userInput.add("errorhandling");
-		userInput.add("errorhandling");
-		userInput.add("handle the error");
-		userInput.add("handle the error somehow");
-		userInput.add("handle this error");
-		userInput.add("yes");
-		
-		this.runMainActivityWithTestInput(userInput);
-		
-		assertTrue(true);
-	}
+//	/**
+//	 * Testing restart, rephrase and restart strategy in CI
+//	 */
+//	@Test
+//	public void reTest1() {
+//		userInput.add("hi");
+//		userInput.add("my name is meng meng");
+//		userInput.add("i want to go to the canteen");
+//		userInput.add("errorhandling");
+//		userInput.add("errorhandling");
+//		userInput.add("errorhandling");
+//		userInput.add("errorhandling");
+//		userInput.add("handle the error");
+//		userInput.add("handle the error somehow");
+//		userInput.add("handle this error");
+//		userInput.add("yes");
+//		
+//		this.runMainActivityWithTestInput(userInput);
+//		
+//		assertTrue(true);
+//	}
+//	
+//	/**
+//	 * Testing restart, rephrase and restart strategy in CI
+//	 */
+//	@Test
+//	public void reTest2() {
+//		userInput.add("hi");
+//		userInput.add("my name is meng meng");
+//		userInput.add("i want to go to the canteen");
+//		userInput.add("can i eat at line one");
+//		userInput.add("errorhandling");
+//		userInput.add("errorhandling");
+//		userInput.add("errorhandling");
+//		userInput.add("errorhandling");
+//		userInput.add("handle the error");
+//		userInput.add("handle the error somehow");
+//		userInput.add("handle this error");
+//		userInput.add("yes");
+//		
+//		this.runMainActivityWithTestInput(userInput);
+//		
+//		assertTrue(true);
+//	}
 	
 	/**
 	 * Testing restart, rephrase and restart strategy in CR
@@ -70,6 +70,7 @@ public class RestartCanteenTest extends BasicTest {
 		userInput.add("handle the error somehow");
 		userInput.add("handle this error");
 		userInput.add("pork");
+		userInput.add("yes");
 		
 		this.runMainActivityWithTestInput(userInput);
 		
@@ -126,14 +127,17 @@ public class RestartCanteenTest extends BasicTest {
 		userInput.add("handle this error");
 		userInput.add("handle this error please");
 		userInput.add("no");
+		userInput.add("i want a recommendation");
 		
 		this.runMainActivityWithTestInput(userInput);
 		
-//		assertTrue((nlgResults.get(3).contains("again") || nlgResults.get(3).contains("repeat"))
-//				&& (nlgResults.get(5).contains("again") || nlgResults.get(5).contains("repeat"))
-//				&& nlgResults.get(6).contains("rephrase")
-//				&& nlgResults.get(8).contains("rephrase")
-//				&& (nlgResults.get(9).contains("would you like") || nlgResults.get(9).contains("prefer")));
+		assertTrue((nlgResults.get(5).contains("again") || nlgResults.get(5).contains("repeat"))
+				&& (nlgResults.get(7).contains("again") || nlgResults.get(7).contains("repeat"))
+				&& nlgResults.get(8).contains("rephrase")
+				&& nlgResults.get(10).contains("rephrase")
+				&& (nlgResults.get(11).contains("you mean") || nlgResults.get(11).contains("you like"))
+				&& (nlgResults.get(12).contains("again") || nlgResults.get(12).contains("repeat"))
+				&& (nlgResults.get(13).contains("would you like") || nlgResults.get(13).contains("prefer")));
 	}
 	
 }
