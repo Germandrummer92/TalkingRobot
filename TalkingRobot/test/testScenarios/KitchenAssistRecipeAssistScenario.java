9bf51863a4c767dece120269b159547c79d32a3a
germandrummer92 <germandrummer92@gmail.com>
2014-03-13 13:55:21 +0100

    scenario changed to work

--- TalkingRobot/test/testScenarios/KitchenAssistRecipeAssistScenario.java
+++ TalkingRobot/test/testScenarios/KitchenAssistRecipeAssistScenario.java
@@ -20,14 +20,17 @@ public class KitchenAssistRecipeAssistScenario extends basicTestCases.BasicTest{
 		userInput.add("a recipe from the usa");
 		userInput.add("tell me the ingredients");
 		userInput.add("what kitchen tools are needed");
+		userInput.add("can you help me in the kitchen now");
 		userInput.add("where can i find a knife");
 		
 		runMainActivityWithTestInput(userInput);
 		
 		assertTrue(nlgResults.get(3).contains("hamburger"));
 		assertTrue(nlgResults.get(4).contains("Ground beef") && nlgResults.get(4).contains("Lettuce") 
-				&& nlgResults.get(4).contains("Buns"));
-		assertTrue(nlgResults.get(5).contains("knife"));
+				&& nlgResults.get(4).contains("Buns")
+				&& nlgResults.get(5).contains("knife")
+				&& nlgResults.get(6).contains("can I"));
+		assertTrue(nlgResults.get(7).contains("silverware drawer"));
 		//TODO does not change from RA to KA
 //		assertTrue(nlgResults.get(6).contains("silverware drawer"));
 	}
