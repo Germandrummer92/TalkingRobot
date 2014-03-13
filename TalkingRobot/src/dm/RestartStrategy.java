599f5427295245439466fdbf8214a2c9807abe26
narigaoka <yue.hong@hotmail.de>
2014-03-13 18:28:54 +0100

    minor error fixed

--- TalkingRobot/src/dm/RestartStrategy.java
+++ TalkingRobot/src/dm/RestartStrategy.java
@@ -119,8 +119,8 @@ public class RestartStrategy extends ErrorStrategy {
 		}
 		
 		randomMeal = meal;
-		String output = "<" + meal.getMealName() + ">,";
-		output = output + "<" + line.getLineName() + ">,";
+		String output = "<" + meal.getMealName() + ">;";
+		output = output + "<" + line.getLineName() + ">;";
 		output = output + "<" + canteenData.getCanteenName().toString().toLowerCase() + ">";
 		return output;
 	}
