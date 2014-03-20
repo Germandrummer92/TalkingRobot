5192ed978c5220f6bbf9ffe79a4e80d96e465c08
rosine <eragonfreak@web.de>
2014-03-20 23:25:17 +0100

    test modified

--- TalkingRobot/test/basicTestCases/CanteenInformationTest.java
+++ TalkingRobot/test/basicTestCases/CanteenInformationTest.java
@@ -2,7 +2,14 @@ package basicTestCases;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
+
+import data.MealData;
+import data.CanteenData;
+import data.CanteenNames;
+import dm.Canteen;
 
 /**
  * 
@@ -18,17 +25,27 @@ public class CanteenInformationTest extends BasicTest{
 	 */
 	@Test
 	public void lineOneTodayAdenExpl() {
+		LinkedList<String> meals = new LinkedList<String>();
+		LinkedList<String> price = new LinkedList<String>();
+		Canteen curCanteen = new Canteen(new CanteenData(CanteenNames.ADENAUERRING, 0));// 0 for "today"
+		for (MealData m : curCanteen.getCanteenData().getLines().get(0).getTodayMeals()) {
+			meals.add(m.getMealName().toLowerCase());
+			price.add(m.getS_price().toString());
+		}
+		
 		userInput.add("hello");
 		userInput.add("i am bettina");
 		userInput.add("what can i eat at line one in canteen adenauerring");
-		userInput.add("how much is Scharfe Sombreropfanne mit Kalbfleischbällchen und Vanillepudding");
-		
-		//TODO das menue ist jetzt von hand reinkopiert. koennte man vllt automatisieren. und die assertion mit abfrage
-		//nach "line xy" funktioniert nicht so großartig, weil es manchmal nicht im output ist
+		userInput.add("how much is " + meals.get(0));
 		
 		this.runMainActivityWithTestInput(userInput);
 		
-		assertTrue(nlgResults.get(2).toLowerCase().contains("at line one"));
+		for (int i = 0; i < meals.size(); i++) {
+			assertTrue(nlgResults.get(2).toLowerCase().contains(meals.get(i)));
+		}
+		if (meals.size() > 0) {
+			assertTrue(nlgResults.get(3).contains(price.get(0)));
+		}
 	}
 	
 	/**
@@ -37,50 +54,85 @@ public class CanteenInformationTest extends BasicTest{
 	 */
 	@Test
 	public void lineOneTodayMoltkeExpl() {
+		LinkedList<String> meals = new LinkedList<String>();
+		LinkedList<String> price = new LinkedList<String>();
+		Canteen curCanteen = new Canteen(new CanteenData(CanteenNames.MOLTKE, 0));// 0 for "today"
+		for (MealData m : curCanteen.getCanteenData().getLines().get(0).getTodayMeals()) {
+			meals.add(m.getMealName().toLowerCase());
+			price.add(m.getS_price().toString());
+		}
+		
 		userInput.add("hello");
 		userInput.add("i am bettina");
 		userInput.add("what can i eat at line one in canteen Moltke");
-		userInput.add("what's the price of Calamares Ringe");
+		userInput.add("what's the price of " + meals.get(0));
 		
-		//TODO das menue ist jetzt von hand reinkopiert. koennte man vllt automatisieren. und die assertion mit abfrage
-		//nach "line xy" funktioniert nicht so großartig, weil es manchmal nicht im output ist
 		this.runMainActivityWithTestInput(userInput);
 		
-		assertTrue(nlgResults.get(2).toLowerCase().contains("at choice one") || nlgResults.get(2).toLowerCase().contains("at line one"));
+		for (int i = 0; i < meals.size(); i++) {
+			assertTrue(nlgResults.get(2).toLowerCase().contains(meals.get(i)));
+		}
+		if (meals.size() > 0) {
+			assertTrue(nlgResults.get(3).contains(price.get(0)));
+		}
 	}
 	
 	/**
-	 * tests for meals at line one in canteen adenauerring next monday.
+	 * tests for meals at line one in canteen adenauerring tomorrow
 	 * everything in one sentence mentioned.
 	 */
 	@Test
-	public void lineOneNextMondayAdenExpl() {
+	public void lineOneTomorrowAdenExpl() {
+		LinkedList<String> meals = new LinkedList<String>();
+		LinkedList<String> price = new LinkedList<String>();
+		Canteen curCanteen = new Canteen(new CanteenData(CanteenNames.ADENAUERRING, 1));// 1 for "tomorrow"
+		for (MealData m : curCanteen.getCanteenData().getLines().get(0).getTodayMeals()) {
+			meals.add(m.getMealName().toLowerCase());
+			price.add(m.getS_price().toString());
+		}
+		
 		userInput.add("hello");
 		userInput.add("i am bettina");
-		userInput.add("what can i eat at line one in canteen adenauerring next monday");
-		userInput.add("whats the price of Oberländer Bratwurst mit Pommes frites");
-		//TODO das menue ist jetzt von hand reinkopiert. koennte man vllt automatisieren. und die assertion mit abfrage
-		//nach "line xy" funktioniert nicht so großartig, weil es manchmal nicht im output ist
+		userInput.add("what can i eat at line one in canteen adenauerring tomorrow");
+		userInput.add("whats the price of " + meals.get(0));
+
 		this.runMainActivityWithTestInput(userInput);
 		
-		
-		assertTrue(nlgResults.get(2).toLowerCase().contains("at line one") );
+		for (int i = 0; i < meals.size(); i++) {
+			assertTrue(nlgResults.get(2).toLowerCase().contains(meals.get(i)));
+		}
+		if (meals.size() > 0) {
+			assertTrue(nlgResults.get(3).contains(price.get(0)));
+		}
 	}
 	
 	/**
-	 * tests for meals at line one in canteen moltke next monday.
+	 * tests for meals at line one in canteen moltke tomorrow.
 	 * everything in one sentence mentioned.
 	 */
 	@Test
-	public void lineOneNextMondayMoltkeExpl() {
+	public void lineOneTomorrowMoltkeExpl() {
+		LinkedList<String> meals = new LinkedList<String>();
+		LinkedList<String> price = new LinkedList<String>();
+		Canteen curCanteen = new Canteen(new CanteenData(CanteenNames.MOLTKE, 1));// 1 for "tomorrow"
+		for (MealData m : curCanteen.getCanteenData().getLines().get(0).getTodayMeals()) {
+			meals.add(m.getMealName().toLowerCase());
+			price.add(m.getS_price().toString());
+		}
+		
 		userInput.add("hello");
 		userInput.add("i am bettina");
-		userInput.add("what can i eat at line one in canteen moltke next monday");
-		userInput.add("how much is Hähnchenbrust im Knuspermantel Bratensoße");
+		userInput.add("what can i eat at line one in canteen moltke tomorrow");
+		userInput.add("how much is " + meals.get(0));
 		
 		this.runMainActivityWithTestInput(userInput);
 		
-		assertTrue(nlgResults.get(2).toLowerCase().contains("hähnchenbrust im knuspermantel bratensoße") || nlgResults.get(2).toLowerCase().contains("langkornreis"));
+		for (int i = 0; i < meals.size(); i++) {
+			assertTrue(nlgResults.get(2).toLowerCase().contains(meals.get(i)));
+		}
+		if (meals.size() > 0) {
+			assertTrue(nlgResults.get(3).contains(price.get(0)));
+		}
 	}
 	
 	/**
